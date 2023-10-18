a6bba7d196c8c9573bb517d905dc9335bd4a36ee
KhantZinMin <khantzinminm@gmail.com>
2023-10-18 10:47:03 +0630

    finished frontend

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -4,6 +4,7 @@ import image from './download.jpeg'
 import { Link } from 'react-router-dom';
 import {
   getLoginStatus,
+  getRoles,
   getUser,
   logout
 } from "../../features/auths/authSlice";
@@ -12,6 +13,8 @@ import { useDispatch, useSelector } from 'react-redux';
 const MainNavigation = () => {
   const loginStatus = useSelector(getLoginStatus);
   const user = useSelector(getUser);
+  const role = useSelector(getRoles)
+  console.log(role);
 
   const dispatch = useDispatch()
 
@@ -27,6 +30,31 @@ const MainNavigation = () => {
   } else {
     navLogin = <Link className="nav-link text-white" to="/user/login">Login</Link>;
   }
+
+  let adminNav = "";
+
+  if (Array.isArray(role) && role.includes("ROLE_ADMIN")) {
+    adminNav = (
+      <>
+    <li className="nav-item">
+    <Link to="/newTicket" className="nav-link text-white">CreateTicket</Link>
+  </li>
+  <li className="nav-item">
+    <Link className="nav-link text-white" to="/allTicket">Tickets</Link>
+  </li>
+  </>
+  )
+  }
+
+  let userNav = "";
+  if (Array.isArray(role) && role.includes("ROLE_USER")) {
+    userNav = (
+      <li className="nav-item">
+      <Link className="nav-link text-white" to="/orders">Your Orders</Link>
+    </li>
+  )
+  }
+
   return (
     <header className={classes.navheader}>
     <div className='container'>
@@ -41,17 +69,10 @@ const MainNavigation = () => {
           <li className="nav-item">
             <Link to="/" className="nav-link active text-white" aria-current="page">Home</Link>
           </li>  
-          <li className="nav-item">
-            <Link to="/newTicket" className="nav-link text-white">NewTicket</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link text-white" to="/allTicket">AllTicket</Link>
-          </li>
+          {adminNav}
           {navProfile}
           <li className="nav-item">{navLogin}</li>
-          <li className="nav-item">
-            <Link className="nav-link text-white" to="/orders">Your Orders</Link>
-          </li>
+          {userNav}
         </ul>
 
         
